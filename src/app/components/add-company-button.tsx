0557996352bf1233b5ecd6adc1fcf9392718c677
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-26 18:08:49 +0200

    task

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,14 +1,23 @@
 'use client';
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import dynamic from 'next/dynamic';
 import Button from '@/app/components/button';
 
+const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+  ssr: false,
+});
+
 export default function AddCompanyButton() {
-  const router = useRouter();
+  const [show, setShow] = useState(false);
   return (
-    <Button onClick={() => router.push('/companies/new', { scroll: false })}>
-      Add company
-    </Button>
+    <>
+      <Button onClick={() => setShow(true)}>Add company</Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => setShow(false)}
+      />
+    </>
   );
 }
